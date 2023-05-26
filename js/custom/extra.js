b49cc8124bd9c83c6335bcccb7538a412fdf1ddb
Burcu Taş <burcutas636@gmail.com>
2023-05-26 19:22:15 +0300

    services section

--- js/custom/extra.js
+++ js/custom/extra.js
@@ -17,9 +17,22 @@ window.addEventListener("DOMContentLoaded", event=>{
 
     const myNavbar = document.body.querySelector("#mainNavbar");
     if(myNavbar){
-        new Bootstrap.ScrollSpy(document.body, {
+        new bootstrap.ScrollSpy(document.body, {
             target: "#mainNavbar",
             offset: 74
         })
     } 
-})
+});
+
+
+//Sayfaki .btn-close-canvas classlarına sahip elemanları bulacak.
+//Daha sonra bu elemanlara tek tek click event'i eklenecek.
+//Yani .btn-close-canvas class'ına sahip olan elemanlara tıklandığında bir fonksiyon gerçekleştirilecek.
+//İlgili elemanlara tıklandığında dokümantasyondaki [data-bs-dismiss="offcanvas"] attribute'una tıklanacak.
+//Böylelikle offcanvas'ı kapatmış olacağız.
+var BtnCanvas = document.querySelectorAll(".btn-close-canvas");
+for (let i = 0; i < BtnCanvas.length; i++) {
+    BtnCanvas[i].addEventListener("click", function(){
+        document.querySelector('[data-bs-dismiss="offcanvas"]').click();
+    });
+}
